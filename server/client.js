2e2a0a1872a35c936e3c87816d3e1977a322afd9
Sachin Dongary <siddharth.sachin89@gmail.com>
2024-04-18 20:44:39 -0500

    Server Commit

--- server/client.js
+++ server/client.js
@@ -1,4 +1,5 @@
 const pg = require('pg');
+
 const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/career_sim_db');
 
 module.exports = {client}
--- server/index.js
+++ server/index.js
@@ -1,41 +1,39 @@
-const {
-  client,
-  createTables,
-  createUser,
-  createProduct,
-  createCartedProducts,
-  updateUser,
-  updateProduct,
-  updateCartedProducts,
-  deleteUser,
-  deleteProduct,
-  deleteCartedProduct,
-  fetchUsers,
-  fetchProducts,
-  fetchCartedProducts,
-  fetchSingleProduct,
-  fetchAllCartedProducts,
-  authenticate,
-  findUserWithToken
-} = require('./DB/seed');
-const express = require('express');
-const { register } = require('module');
-const {fakeData} = require('./DB/data');
-const path = require('path');
+
+// const express = require('express');
+// const { register } = require('module');
+// 
+
+const { client } = require('./client.js');
+const {fakeData} = require('./DB/data.js');
+const {createTables} = require('./DB/seed.js');
+
+const cors = require('cors');
+
 const app = express();
 app.use(express.json());
 
+const usersRouter = require('./API/users.js');
+const productsRouter = require('./API/products.js');
+const cartsRouter = require('./API/carts.js');
+const checkoutRouter = require('./API/checkout.js')
+
 
 //for deployment only
-
+const path = require('path');
 app.get('/', (req, res)=> res.sendFile(path.join(__dirname, '../client/dist/index.html')));
 app.use('/assets', express.static(path.join(__dirname, '../client/dist/assets'))); 
 
 
-app.use((err, req, res, next)=> {
-  console.log(err);
-  res.status(err.status || 500).send({ error: err.message ? err.message : err });
-});
+// app.use((err, req, res, next)=> {
+//   console.log(err);
+//   res.status(err.status || 500).send({ error: err.message ? err.message : err });
+// });
+
+app.use(cors());
+app.use('/API/users, usersRouter');
+app.use('/API/products, productssRouter');
+app.use('/API/cart, cartsRouter');
+app.use('/API/checkout, checkoutRouter');
 
 const init = async()=> {
   const port = process.env.PORT || 3000;
